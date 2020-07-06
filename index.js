5161dc5378ab9365230c3d639a2a90ce08989b68
Benedict Tobias <benedict.tobias@yahoo.com>
2020-07-06 12:24:05 -0400

    example of promises in node

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ process.on('exit', () => {
 
 // create an event emitter
 const { EventEmitter } = require('events'); //build-in
+const { read } = require('fs');
 
 // Create an instance of the emitter
 const eventEmitter = new EventEmitter();
@@ -27,12 +28,22 @@ eventEmitter.emit('lunch');
 eventEmitter.emit('lunch');
 
 // File system example (sync)
-const {readFile, readFileSync } = require('fs'); // fs is a build-in module
-const text = readFileSync('./file.txt', 'utf8');
-console.log(text);
+// const {readFile, readFileSync } = require('fs'); // fs is a build-in module
+// const text = readFileSync('./file.txt', 'utf8');
+// console.log(text);
 
 // async file read
-const textAsync = readFile('./file.txt', 'utf8', (err, txt) => {
-    console.log(txt);
-})
-console.log('this log should come before the async read');
+// const textAsync = readFile('./file.txt', 'utf8', (err, txt) => {
+//     console.log(txt);
+// })
+// console.log('this log should come before the async read');
+
+const {readFile} = require('fs').promises;
+async function readIt() {
+    var text = await readFile('./file.txt', 'utf8');
+    console.log(text);
+}
+
+console.log("before read"); // executed first
+readIt();                   // executed second, but finished last
+console.log("after read"); // executed third
