3ead4017f65f1ea8247a7488c30526a6472b2a73
Benedict Tobias <benedict.tobias@yahoo.com>
2020-07-06 15:35:40 -0400

    express listen example

--- index.js
+++ index.js
@@ -1,57 +1,15 @@
-console.log('test output');
+const {readFile} = require('fs');
+const express = require('express');
+const app = new express();
 
-// example of global variable
-global.myOwnGlobalVariable = 'global variable';
-console.log(myOwnGlobalVariable);
+app.get('/', (request, response) => {
+    readFile('./home.html', 'utf8', (err, html) => {
+        if (err){
+            response.status(500).send('sorry!');
+        }
 
-// example of process
-console.log(process.env);
-console.log(`Process: ${process.platform}` );
-
-// example of events
-// one of the example of event is 'exit' (build-in to node)
-process.on('exit', () => {
-    console.log('bye now!');
+        response.send(html);
+    })
 });
 
-// create an event emitter
-const { EventEmitter } = require('events'); //build-in
-const { read } = require('fs');
-
-// Create an instance of the emitter
-const eventEmitter = new EventEmitter();
-eventEmitter.on('lunch', () => {
-    console.log('hmm.. yummy!');
-});
-
-eventEmitter.emit('lunch');
-eventEmitter.emit('lunch');
-
-// File system example (sync)
-// const {readFile, readFileSync } = require('fs'); // fs is a build-in module
-// const text = readFileSync('./file.txt', 'utf8');
-// console.log(text);
-
-// async file read
-// const textAsync = readFile('./file.txt', 'utf8', (err, txt) => {
-//     console.log(txt);
-// })
-// console.log('this log should come before the async read');
-
-const {readFile} = require('fs').promises;
-async function readIt() {
-    var text = await readFile('./file.txt', 'utf8');
-    console.log(text);
-}
-
-console.log("before read"); // executed first
-readIt();                   // executed second, but finished last
-console.log("after read"); // executed third
-
-// Example of 'empty' module
-const myModule = require('./my-module');
-console.log(myModule);
-
-// Example of 'new' module
-const myNewModule = require('./my-new-module');
-console.log(myNewModule);
+app.listen(process.env.port || 3000, () => {console.log('app is up!')})
