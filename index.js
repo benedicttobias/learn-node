a700764a866966021b090e2fc6815c3ce00a1614
Benedict Tobias <benedict.tobias@yahoo.com>
2020-07-06 12:04:47 -0400

    add example of getting process variable

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 console.log('test output');
 
+// example of global variable
 global.myOwnGlobalVariable = 'global variable';
-
 console.log(myOwnGlobalVariable);
+
+// example of process
+console.log(process.env);
+console.log(`Process: ${process.platform}` );
