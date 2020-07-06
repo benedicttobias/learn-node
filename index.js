3a11637c15323bbb701a3f2dff7a50bbdff21d74
Benedict Tobias <benedict.tobias@yahoo.com>
2020-07-06 12:13:29 -0400

    add event example

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ console.log(myOwnGlobalVariable);
 // example of process
 console.log(process.env);
 console.log(`Process: ${process.platform}` );
+
+// example of events
+// one of the example of event is 'exit' (build-in to node)
+process.on('exit', () => {
+    console.log('bye now!');
+});
+
+// create an event emitter
+const { EventEmitter } = require('events'); //build-in
+
+// Create an instance of the emitter
+const eventEmitter = new EventEmitter();
+eventEmitter.on('lunch', () => {
+    console.log('hmm.. yummy!');
+});
+
+eventEmitter.emit('lunch');
+eventEmitter.emit('lunch');
